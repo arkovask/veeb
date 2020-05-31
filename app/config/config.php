a783b13e97243c6a9c388e001151de48e16ea621
arkovask <48015320+arkovask@users.noreply.github.com>
2020-05-31 17:14:25 +0300

    Add files via upload

---
+++ app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+// rakendusega seotud konstandid
+define('URLROOT', 'http://localhost/veeb');
+define('APPROOT', dirname(dirname(__FILE__)));
+define('SITENAME', 'IS Veeb');
+
+// db konstandid
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'veeb');
